89af7b1d7d25caa78e4e874e2b328e1812687f18
User <anastasia.moon2000@gmail.com>
2023-11-27 12:07:29 +0300

    README.md changed

---
+++ testTemplates/testTemplates/views.py
@@ -0,0 +1,19 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+MENU = {"главная":"/", "статья":"/state", "о нас":"/about"}
+
+def main_page(request):
+    title = ""
+    data = {"menu":MENU, "title": title}
+    return render(request,"./index.html", context=data)
+
+def state_page(request):
+    title = "Статья"
+    data = {"menu":MENU, "title": title}
+    return render(request,"./state.html", context=data)
+
+def about_page(request):
+    title = "О нас"
+    data = {"menu":MENU, "title": title}
+    return render(request,"./about.html", context=data)
